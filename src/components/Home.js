4df201bccedb7132521760479819b0ccc58d30b1
Nagendra Anala <Nagendraanala@gmail.com>
2023-09-15 00:04:41 +0530

    first publish

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,24 +6,25 @@ import CryptoJS from "crypto-js";
 
 
 const Home = () => {
+    // U2FsdGVkX1/PtCjVEm5pjzX2VjELF7rOfT/1SC1hjxi2PXik6NLi7hHDOuLc4F3x
     const [text, settext] = useState("");
     const [encrptedData, setEncrptedData] = useState("");
     const [decrptedData, setDecrptedData] = useState("");
   
     const secretPass = "Minato_Namekaze_is_the_goat";
-    const delay = ms => new Promise(
-        resolve => setTimeout(resolve, ms)
-    );
-    const reroute = async () => {
-        decryptData() ;
-        console.log('reroute' + decrptedData);
-        const latitude= decrptedData.substring(0,10);
-        const longitude = decrptedData.substring(10,20);
-        // console.log('reroute');
-        console.log('reroute'+latitude);
-        console.log(longitude);
-        window.open(`https://maps.google.com/?q=${latitude},${longitude}`);
-    }
+    // const delay = ms => new Promise(
+    //     resolve => setTimeout(resolve, ms)
+    // );
+    // const reroute = async () => {
+    //     decryptData() ;
+    //     console.log('reroute' + decrptedData);
+    //     const latitude= decrptedData.substring(0,10);
+    //     const longitude = decrptedData.substring(10,20);
+    //     // console.log('reroute');
+    //     console.log('reroute'+latitude);
+    //     console.log(longitude);
+    //     window.open(`https://maps.google.com/?q=${latitude},${longitude}`);
+    // }
   
     const encryptData = (d) => {
       const data = CryptoJS.AES.encrypt(
@@ -37,8 +38,14 @@ const Home = () => {
     const decryptData = async () => {
       const bytes = CryptoJS.AES.decrypt(text, secretPass);
       const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-      setDecrptedData(data);
-      await delay (1000);
+    //   setDecrptedData(data);
+    //   await delay (1000);
+        const latitude= data.substring(0,10);
+        const longitude = data.substring(10,20);
+        // console.log('reroute');
+        console.log('reroute'+latitude);
+        console.log(longitude);
+        window.open(`https://maps.google.com/?q=${latitude},${longitude}`);
     };
 
     const handleLocationClick = () => 
@@ -65,18 +72,17 @@ const Home = () => {
   return (
     <div className='home'>
         <div className='text'>
-        {/* <TextField sx={{ width: '40%', "& fieldset": { border: 'none' },borderRadius: '15px', backgroundColor: 'whitesmoke',boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' , marginBottom: '20px'}} rows={8} multiline
+        <TextField sx={{ width: '40%', "& fieldset": { border: 'none' },borderRadius: '15px', backgroundColor: 'whitesmoke',boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' , marginBottom: '20px'}} rows={8} multiline
         value = {text}
         onChange={(e) => {
             settext(e.target.value);
             console.log(text);
         }}
-        /> */}
-        {text}
+        />
         </div>
 
         <div><Button sx={{backgroundColor: '#0471c4' , color: 'white' , marginTop: '2vh' }} className='get-code'  onClick={handleLocationClick}> Get Code </Button></div>
-        <div><Button sx={{backgroundColor: '#0471c4' , color: 'white', marginTop: '2vh'}} className='get-location' onClick={reroute}> Get Location </Button></div>
+        <div><Button sx={{backgroundColor: '#0471c4' , color: 'white', marginTop: '2vh'}} className='get-location' onClick={decryptData}> Get Location </Button></div>
     </div>
   )
 }
